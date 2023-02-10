4af3609896eb56a330dc0956010a868e8a3673a6
chrismclarke <chris.m.clarke@live.co.uk>
2023-02-10 09:49:43 -0800

    chore: update config

--- config.ts
+++ config.ts
@@ -1,21 +1,17 @@
-import { IDeploymentConfig, getDefaultAppConstants } from "data-models";
-
-const app_constants = getDefaultAppConstants();
+import { generateDeploymentConfig } from "scripts";
+const config = generateDeploymentConfig("c2dev_demo");
 
 // Override any app constants here
-app_constants.APP_HEADER_DEFAULTS.title = "c2dev";
-app_constants.APP_SIDEMENU_DEFAULTS.title = "c2dev";
+config.app_config.APP_HEADER_DEFAULTS.title = "c2dev";
+config.app_config.APP_SIDEMENU_DEFAULTS.title = "c2dev";
 
-const config: IDeploymentConfig = {
-  name: "c2dev_demo",
-  git: {
-    content_repo: "https://github.com/c-squared-dev/idems-app-c2dev-demo",
-    content_tag_latest: "1.0.11",
-  },
-  google_drive: {
-    sheets_folder_id: "1ja6lzbphZaxnVv5mpQ4YHnn2qmxMiEBW",
-    assets_folder_id: "1bNvUKN47YZAbMnRA1ThzSLGxLTd0mfDb",
-  },
-  app_constants,
+config.git = {
+  content_repo: "https://github.com/c-squared-dev/idems-app-c2dev-demo",
+  content_tag_latest: "1.0.11",
 };
+config.google_drive = {
+  sheets_folder_id: "1ja6lzbphZaxnVv5mpQ4YHnn2qmxMiEBW",
+  assets_folder_id: "1bNvUKN47YZAbMnRA1ThzSLGxLTd0mfDb",
+};
+
 export default config;
