2f917913f8fc33b7783248dbccd1d03b9d55817b
Chris <chris.m.clarke@live.co.uk>
2023-05-03 23:18:02 +0100

    chore: update config

--- config.ts
+++ config.ts
@@ -2,16 +2,16 @@ import { generateDeploymentConfig } from "scripts";
 const config = generateDeploymentConfig("debug_git")
 
 config.google_drive = {
-  // This is the quality assurance sheets used in plh
-  sheets_folder_id: "1YcG0GDJME1Ds1UzwXcEmo97Ckb5tnUWc",
-  // global quality assurance folder only
-  assets_folder_id: "1bnGPB70wPp9hzVOpT4LkPX9h3FwIrqfs",
+  sheets_folder_id: "1ja6lzbphZaxnVv5mpQ4YHnn2qmxMiEBW",
+  assets_folder_id: "1bNvUKN47YZAbMnRA1ThzSLGxLTd0mfDb",
 }
 
 config.git = {
   content_repo:'https://github.com/c-squared-dev/idems-app-debug-git',
-  content_tag_latest:'1.0.0'
+  content_tag_latest:"1.1.0"
 }
+
+config.app_data.output_path = './app_data'
 
 // Override any app constants here
 config.app_config.APP_HEADER_DEFAULTS.title = "debug_git"
